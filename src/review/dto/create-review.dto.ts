7f270beb021cda00b1fe4414bec8f08b499803ff
Maksim Borovik <borovik.maksim@gmail.com>
2024-04-13 17:40:09 +0300

    add validation for review dto

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,20 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+	@IsString()
 	name: string;
+
+	@IsString()
 	title: string;
+
+	@IsString()
 	description: string;
+
+	@Max(5, { message: 'Rating can\'t be more than $constraint1' })
+	@Min(1, { message: 'Rating can\'t be less than $constraint1' })
+	@IsNumber()
 	rating: number;
+
+	@IsString()
 	productId: string;
 }
