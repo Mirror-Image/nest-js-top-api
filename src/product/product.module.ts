a77a6b13d68ade23caad6459018ea3abcdc51945
Maksim Borovik <borovik.maksim@gmail.com>
2024-04-16 02:46:57 +0300

    add aggregation

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
 import { KindagooseModule } from 'kindagoose';
 import { ProductModel } from './product.model';
+import { ProductService } from './product.service';
 
 @Module({
   controllers: [ProductController],
   imports: [KindagooseModule.forFeature([ProductModel])],
+  providers: [ProductService],
 })
 export class ProductModule {}
