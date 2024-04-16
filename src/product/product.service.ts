21b08ccd5555c694619afeaf9146c5ee32eb4f03
Maksim Borovik <borovik.maksim@gmail.com>
2024-04-16 23:09:01 +0300

    add custom pipe and custom mongoBd aggregate function

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -56,7 +56,18 @@ export class ProductService {
 			{
 				$addFields: {
 					reviewCount: { $size: '$reviews' },
-					reviewAvg: { $avg: '$reviews.rating' }
+					reviewAvg: { $avg: '$reviews.rating' },
+					// MongoServerError: $function not allowed in this atlas tier
+					// reviews: {
+					// 	$function: {
+					// 		body: `function (reviews) {
+					// 			reviews.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+					// 			return reviews;
+					// 		}`,
+					// 		args: ['$reviews'],
+					// 		lang: 'js',
+					// 	}
+					// }
 				}
 			}
 		]).exec() as (ProductModel & {
