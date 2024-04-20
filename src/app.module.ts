d098bcf378468871685a98342c6dd06d726eb3e9
Maksim Borovik <borovik.maksim@gmail.com>
2024-04-20 21:27:56 +0300

    add telegram bot api integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { KindagooseModule } from 'kindagoose';
 import { getMongoConfig } from './configs/mongo.config';
 import { FilesModule } from './files/files.module';
 import { SitemapModule } from './sitemap/sitemap.module';
+import { TelegramModule } from './telegram/telegram.module';
+import { getTelegramConfig } from './configs/telegram.config';
 
 @Module({
 	imports: [
@@ -22,7 +24,12 @@ import { SitemapModule } from './sitemap/sitemap.module';
 		ProductModule,
 		ReviewModule,
 		FilesModule,
-		SitemapModule
+		SitemapModule,
+		TelegramModule.forRootAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getTelegramConfig,
+		}),
 	],
 })
 export class AppModule {}
--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -3,10 +3,14 @@ import { ReviewController } from './review.controller';
 import { KindagooseModule } from 'kindagoose';
 import { ReviewModel } from './review.model';
 import { ReviewService } from './review.service';
+import { TelegramModule } from '../telegram/telegram.module';
 
 @Module({
   controllers: [ReviewController],
-  imports: [KindagooseModule.forFeature([ReviewModel])],
+  imports: [
+	KindagooseModule.forFeature([ReviewModel]),
+	TelegramModule,
+  ],
   providers: [ReviewService]
 })
 export class ReviewModule {}
